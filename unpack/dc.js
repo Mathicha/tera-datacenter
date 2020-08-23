780b40b4d9c8a8573ae532ee675af5139fd536d2
Mathi <11136549+Mathicha@users.noreply.github.com>
2020-08-23 19:41:20 +0200

    64-bit fix

--- unpack/dc.js
+++ unpack/dc.js
@@ -1,249 +1,277 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const zlib = require('zlib');
-const crypto = require('crypto');
+const fs = require("fs");
+const zlib = require("zlib");
+const crypto = require("crypto");
 
 function decrypt(DataCenterPath, key, iv) {
-	if (key.length !== 32) throw Error('Invalid key length');
-	if (iv.length !== 32) throw Error('Invalid IV length');
+  if (key.length !== 32) throw Error("Invalid key length");
+  if (iv.length !== 32) throw Error("Invalid IV length");
 
-	const original = fs.readFileSync(DataCenterPath);
-	console.log('original size', original.length);
-	console.log('original sha256', sha(original));
+  const original = fs.readFileSync(DataCenterPath);
+  console.log("original size", original.length);
+  console.log("original sha256", sha(original));
 
-	const decipher = crypto.createDecipheriv('aes-128-cfb', StringToByteArray(key), StringToByteArray(iv));
-	let decrypted = decipher.update(original);
-	decipher.final();
-	console.log('decrypted size', decrypted.length);
-	console.log('decrypted sha256', sha(decrypted));
+  const decipher = crypto.createDecipheriv(
+    "aes-128-cfb",
+    Buffer.from(key, "hex"),
+    Buffer.from(iv, "hex")
+  );
+  let decrypted = decipher.update(original);
+  decipher.final();
+  console.log("decrypted size", decrypted.length);
+  console.log("decrypted sha256", sha(decrypted));
 
-	if (decrypted.readUInt16LE(4) !== 0x9c78) throw Error('Incorrect key/iv');
+  if (decrypted.readUInt16LE(4) !== 0x9c78) throw Error("Incorrect key/iv");
 
-	let unpacked = zlib.inflateSync(decrypted.slice(4, decrypted.length));
-	console.log('unpacked size', unpacked.length);
-	console.log('unpacked sha256', sha(unpacked));
+  let unpacked = zlib.inflateSync(decrypted.slice(4, decrypted.length));
+  console.log("unpacked size", unpacked.length);
+  console.log("unpacked sha256", sha(unpacked));
 
-	return unpacked;
+  return unpacked;
 }
 
 class DCReader {
-	constructor(buffer) {
-		this.buffer = buffer;
-		this.offset = 0;
+  constructor(buffer) {
+    this.buffer = buffer;
+    this.offset = 0;
 
-		this.TypeCode = new Map()
-			.set(1, () => {
-				return this.UInt32();
-			})
-			.set(2, () => {
-				return this.Float();
-			})
-			.set(5, () => {
-				const value = this.UInt32();
-				if (![0, 1].includes(value)) throw Error('Attributes(): not a bool', value);
-				return Boolean(value);
-			});
-	}
+    this.TypeCode = new Map()
+      .set(1, () => {
+        return this.UInt32();
+      })
+      .set(2, () => {
+        return this.Float();
+      })
+      .set(5, () => {
+        const value = this.UInt32();
+        if (![0, 1].includes(value))
+          throw Error("Attributes(): not a bool", value);
+        return Boolean(value);
+      });
+  }
 
-	parse({ debug = false, mapStrs = false }) {
-		mapStrs = mapStrs ? ['Strings', 'Names'] : [];
+  parse({ debug = false, mapStrs = false }) {
+    mapStrs = mapStrs ? ["Strings", "Names"] : [];
 
-		if (debug) console.time('DCReader.parse()');
-		const parsed = {};
+    if (debug) console.time("DCReader.parse()");
+    const parsed = {};
 
-		if (debug) console.log('DCReader.parse(): Reading Header');
-		parsed.Header = this.Header();
+    if (debug) console.log("DCReader.parse(): Reading Header");
+    parsed.Header = this.Header();
 
-		if (debug) console.log('DCReader.parse(): Reading Unk1');
-		parsed.Unk1 = this.Region({ type: 'Unk1' });
+    if (debug) console.log("DCReader.parse(): Reading Unk1");
+    parsed.Unk1 = this.Region({ type: "Unk1" });
 
-		if (debug) console.log('DCReader.parse(): Reading Attributes');
-		parsed.Attributes = this.Region({ type: 'Region', opts: { type: 'Attributes', writeactual: true } });
+    if (debug) console.log("DCReader.parse(): Reading Attributes");
+    parsed.Attributes = this.Region({
+      type: "Region",
+      opts: { type: "Attributes", writeactual: true },
+    });
 
-		if (debug) console.log('DCReader.parse(): Reading Elements');
-		parsed.Elements = this.Region({ type: 'Region', opts: { type: 'Elements', writeactual: true } });
+    if (debug) console.log("DCReader.parse(): Reading Elements");
+    parsed.Elements = this.Region({
+      type: "Region",
+      opts: { type: "Elements", writeactual: true },
+    });
 
-		if (debug) console.log('DCReader.parse(): Reading Strings');
-		parsed.Strings = this.StringRegion(1024);
+    if (debug) console.log("DCReader.parse(): Reading Strings");
+    parsed.Strings = this.StringRegion(1024);
 
-		if (mapStrs.includes('Strings')) {
-			if (debug) console.log('DCReader.parse(): Mapping Strings');
-			parsed.Strings.map = new Map();
-			for (const index in parsed.Strings.values.data) {
-				const strings = parsed.Strings.values.data[index].value.split('\0');
-				let size = 0;
-				for (let string in strings) {
-					parsed.Strings.map.set(`${index},${size}`, strings[string]);
-					size += strings[string].length + 1;
-				}
-			}
-		}
+    if (mapStrs.includes("Strings")) {
+      if (debug) console.log("DCReader.parse(): Mapping Strings");
+      parsed.Strings.map = new Map();
+      for (const index in parsed.Strings.values.data) {
+        const strings = parsed.Strings.values.data[index].value.split("\0");
+        let size = 0;
+        for (let string in strings) {
+          parsed.Strings.map.set(`${index},${size}`, strings[string]);
+          size += strings[string].length + 1;
+        }
+      }
+    }
 
-		if (debug) console.log('DCReader.parse(): Reading Names');
-		parsed.Names = this.StringRegion(512);
+    if (debug) console.log("DCReader.parse(): Reading Names");
+    parsed.Names = this.StringRegion(512);
 
-		if (mapStrs.includes('Names')) {
-			if (debug) console.log('DCReader.parse(): Mapping Names');
-			parsed.Names.map = new Map();
-			for (const index in parsed.Names.values.data) {
-				const names = parsed.Names.values.data[index].value.split('\0');
-				let size = 0;
-				for (let name in names) {
-					parsed.Names.map.set(`${index},${size}`, names[name]);
-					size += names[name].length + 1;
-				}
-			}
-		}
+    if (mapStrs.includes("Names")) {
+      if (debug) console.log("DCReader.parse(): Mapping Names");
+      parsed.Names.map = new Map();
+      for (const index in parsed.Names.values.data) {
+        const names = parsed.Names.values.data[index].value.split("\0");
+        let size = 0;
+        for (let name in names) {
+          parsed.Names.map.set(`${index},${size}`, names[name]);
+          size += names[name].length + 1;
+        }
+      }
+    }
 
-		if (this.buffer.length === this.offset) {
-			console.log('DCReader.parse(): Missing Footer!'); // yo pinkie why are u deleting the footer in ur dc mod
-			parsed.Footer = { unk1: 0 };
-		} else {
-			if (debug) console.log('DCReader.parse(): Reading Footer');
-			parsed.Footer = this.Footer();
-		}
+    if (this.buffer.length === this.offset) {
+      console.log("DCReader.parse(): Missing Footer!"); // yo pinkie why are u deleting the footer in ur dc mod
+      parsed.Footer = { unk1: 0 };
+    } else {
+      if (debug) console.log("DCReader.parse(): Reading Footer");
+      parsed.Footer = this.Footer();
+    }
 
-		if (this.buffer.length === this.offset) {
-			if (debug) {
-				console.log('DCReader.parse(): Done!');
-				console.timeEnd('DCReader.parse()');
-			}
-			return parsed;
-		} else {
-			throw Error(`ERR @ DCReader.parse(): ${this.buffer.length - this.offset} bytes left`);
-		}
-	}
+    if (this.buffer.length === this.offset) {
+      if (debug) {
+        console.log("DCReader.parse(): Done!");
+        console.timeEnd("DCReader.parse()");
+      }
+      return parsed;
+    } else {
+      throw Error(
+        `ERR @ DCReader.parse(): ${this.buffer.length - this.offset} bytes left`
+      );
+    }
+  }
 
-	Header() {
-		let unk1 = this.UInt32();
-		let unk2 = this.UInt32();
-		let unk3 = this.UInt32();
-		let version = this.UInt32();
-		let unk4 = this.UInt32();
-		let unk5 = this.UInt32();
-		let unk6 = this.UInt32();
-		let unk7 = this.UInt32();
-		return { unk1, unk2, unk3, version, unk4, unk5, unk6, unk7 };
-	}
+  Header() {
+    let unk1 = this.UInt32();
+    let unk2 = this.UInt32();
+    let unk3 = this.UInt32();
+    let version = this.UInt32();
+    let unk4 = this.UInt32();
+    let unk5 = this.UInt32();
+    let unk6 = this.UInt32();
+    let unk7 = this.UInt32();
+    return { unk1, unk2, unk3, version, unk4, unk5, unk6, unk7 };
+  }
 
-	Unk1() {
-		return [this.UInt32(), this.UInt32()];
-	}
+  Unk1() {
+    return [this.UInt32(), this.UInt32()];
+  }
 
-	Address() {
-		return [this.UInt16(), this.UInt16()];
-	}
+  Address() {
+    return [this.UInt16(), this.UInt16()];
+  }
 
-	Attributes() {
-		let name_index = this.UInt16();
-		let type = this.UInt16();
-		let value = null;
-		if (this.TypeCode.has(type)) value = this.TypeCode.get(type)();
-		else value = this.Address();
-		return { name_index, type, value };
-	}
+  Attributes() {
+    let name_index = this.UInt16();
+    let type = this.UInt16();
+    let value = null;
+    if (this.TypeCode.has(type)) value = this.TypeCode.get(type)();
+    else value = this.Address();
+    this.offset += 4; // padding, 64-bit only
+    return { name_index, type, value };
+  }
 
-	Elements() {
-		let name_index = this.UInt16();
-		let unk1 = this.UInt16();
-		let attribute_count = this.UInt16();
-		let children_count = this.UInt16();
-		let attributes = this.Address();
-		let children = this.Address();
-		return { name_index, unk1, attribute_count, children_count, attributes, children };
-	}
+  Elements() {
+    let name_index = this.UInt16();
+    let unk1 = this.UInt16();
+    let attribute_count = this.UInt16();
+    let children_count = this.UInt16();
+    let attributes = this.Address();
+    this.offset += 4; // padding, 64-bit only
+    let children = this.Address();
+    this.offset += 4; // padding, 64-bit only
+    return {
+      name_index,
+      unk1,
+      attribute_count,
+      children_count,
+      attributes,
+      children,
+    };
+  }
 
-	StringRegion(size) {
-		let values = this.Region({ type: 'Str' });
-		let metadata = this.Region({ type: 'Region', opts: { type: 'Meta' }, size, writesize: false });
-		let addresses = this.Region({ type: 'Address', offby: -1 });
-		return { values, metadata, addresses };
-	}
+  StringRegion(size) {
+    let values = this.Region({ type: "Str" });
+    let metadata = this.Region({
+      type: "Region",
+      opts: { type: "Meta" },
+      size,
+      writesize: false,
+    });
+    let addresses = this.Region({ type: "Address", offby: -1 });
+    return { values, metadata, addresses };
+  }
 
-	Str() {
-		let size = this.UInt32();
-		let used = this.UInt32();
-		let value = this.slice(size * 2).toString('ucs2');
-		return { size, used, value };
-	}
+  Str() {
+    let size = this.UInt32();
+    let used = this.UInt32();
+    let value = this.slice(size * 2).toString("ucs2");
+    return { size, used, value };
+  }
 
-	Meta() {
-		let unk1 = this.UInt32();
-		let length = this.UInt32();
-		let id = this.UInt32();
-		let address = this.Address();
-		return { unk1, length, id, address };
-	}
+  Meta() {
+    let unk1 = this.UInt32();
+    let length = this.UInt32();
+    let id = this.UInt32();
+    let address = this.Address();
+    return { unk1, length, id, address };
+  }
 
-	Footer() {
-		return [this.UInt32()];
-	}
+  Footer() {
+    return [this.UInt32()];
+  }
 
-	Region({ type = [], opts = {}, size = 0, actual = 0, offby = 0, writesize = true, writeactual = false, debug = false }) {
-		let data = {};
+  Region({
+    type = [],
+    opts = {},
+    size = 0,
+    actual = 0,
+    offby = 0,
+    writesize = true,
+    writeactual = false,
+    debug = false,
+  }) {
+    let data = {};
 
-		if (writesize) {
-			size = this.UInt32();
-			if (debug) console.log('DCReader.Region(): writesize', size);
-		}
-		if (writeactual) {
-			actual = this.UInt32();
-			if (debug) console.log('DCReader.Region(): writeactual', actual);
-		}
+    if (writesize) {
+      size = this.UInt32();
+      if (debug) console.log("DCReader.Region(): writesize", size);
+    }
+    if (writeactual) {
+      actual = this.UInt32();
+      if (debug) console.log("DCReader.Region(): writeactual", actual);
+    }
 
-		size += offby;
+    size += offby;
 
-		let arr = [];
+    let arr = [];
 
-		for (let i = 0; i < size; i++) arr.push(this[type](opts));
+    for (let i = 0; i < size; i++) arr.push(this[type](opts));
 
-		if (writesize) data.size = size;
-		if (writeactual) data.actual = actual;
-		if (arr[0]) data.data = arr;
+    if (writesize) data.size = size;
+    if (writeactual) data.actual = actual;
+    if (arr[0]) data.data = arr;
 
-		return data;
-	}
+    return data;
+  }
 
-	slice(length) {
-		let data = this.buffer.slice(this.offset, this.offset + length);
-		this.offset += length;
-		return data;
-	}
+  slice(length) {
+    let data = this.buffer.slice(this.offset, this.offset + length);
+    this.offset += length;
+    return data;
+  }
 
-	Float(offset) {
-		if (offset) this.offset = offset;
-		let data = this.buffer.readFloatLE(this.offset);
-		this.offset += 4;
-		return data;
-	}
+  Float(offset) {
+    if (offset) this.offset = offset;
+    let data = this.buffer.readFloatLE(this.offset);
+    this.offset += 4;
+    return data;
+  }
 
-	UInt16(offset) {
-		if (offset) this.offset = offset;
-		let data = this.buffer.readUInt16LE(this.offset);
-		this.offset += 2;
-		return data;
-	}
+  UInt16(offset) {
+    if (offset) this.offset = offset;
+    let data = this.buffer.readUInt16LE(this.offset);
+    this.offset += 2;
+    return data;
+  }
 
-	UInt32(offset) {
-		if (offset) this.offset = offset;
-		let data = this.buffer.readUInt32LE(this.offset);
-		this.offset += 4;
-		return data;
-	}
+  UInt32(offset) {
+    if (offset) this.offset = offset;
+    let data = this.buffer.readUInt32LE(this.offset);
+    this.offset += 4;
+    return data;
+  }
 }
 
 module.exports = { decrypt, DCReader };
 
-function StringToByteArray(str) {
-	let buf = Buffer.alloc(16);
-	for (let i = 0; i < str.length; i += 2) buf[i / 2] = Buffer.from(str.substr(i, 2), 'hex')[0];
-	return buf;
-}
-
 function sha(data) {
-	return crypto
-		.createHash('SHA256')
-		.update(data)
-		.digest('hex');
+  return crypto.createHash("SHA256").update(data).digest("hex");
 }
--- unpack/unpack.js
+++ unpack/unpack.js
@@ -1,18 +1,19 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const { DCReader, decrypt } = require('./dc');
+const fs = require("fs");
+const path = require("path");
+const { DCReader, decrypt } = require("./dc");
 
-const key = '3f70d21d68a9387957e40e40c6064f6b';
-const iv = 'ebf3914c38a2ee376862c333d202f36a';
+const key = "1C01C904FF76FF06C211187E197B5716";
+const iv = "396C342C52A0C12D511DD0209F90CA7D";
 
-const DCBasePath = 'C:\\Program Files (x86)\\Steam\\steamapps\\common\\TERA\\Client\\S1Game\\S1Data'; // <- Path to S1Data
-const DCLang = 'EUR'; // 'USA' for NA
+const DCBasePath =
+  "H:\\Steam\\steamapps\\common\\TERA\\Client\\S1Game\\S1Data"; // <- Path to S1Data
+const DCLang = "EUR"; // 'USA' for NA
 
 const DCPath = path.join(DCBasePath, `DataCenter_Final_${DCLang}.dat`);
 
-const output = path.join(__dirname, '..', 'out', DCLang);
+const output = path.join(__dirname, "..", "out", DCLang);
 
 const unpacked = decrypt(DCPath, key, iv);
 const DC = new DCReader(unpacked);
@@ -20,73 +21,87 @@ const DataCenter = DC.parse({ debug: true, mapStrs: true });
 
 const files = get_files(DataCenter.Elements.data[0].data[0]);
 
-console.time('im done');
-Object.keys(files).forEach(file => {
-	if (Array.isArray(files[file])) {
-		console.log(file, `(x${files[file].length})`);
-		for (let n in files[file]) fs.writeFileSync(path.join(output, file, `${file}-${n}.json`), JSON.stringify(build(files[file][n]), null, '\t'));
-	} else {
-		console.log(file);
-		fs.writeFileSync(path.join(output, file + '.json'), JSON.stringify(build(files[file]), null, '\t'));
-	}
+console.time("im done");
+Object.keys(files).forEach((file) => {
+  if (Array.isArray(files[file])) {
+    console.log(file, `(x${files[file].length})`);
+    for (let n in files[file])
+      fs.writeFileSync(
+        path.join(output, file, `${file}-${n}.json`),
+        JSON.stringify(build(files[file][n]), null, "\t")
+      );
+  } else {
+    console.log(file);
+    fs.writeFileSync(
+      path.join(output, file + ".json"),
+      JSON.stringify(build(files[file]), null, "\t")
+    );
+  }
 });
-console.timeEnd('im done');
+console.timeEnd("im done");
 
 function build(elem) {
-	let obj = {};
+  let obj = {};
 
-	if (elem.attribute_count > 0)
-		for (let i = 0; i < elem.attribute_count; i++) {
-			let ref = DataCenter.Attributes.data[elem.attributes[0]].data[elem.attributes[1] + i];
-			let key = get_Name(ref);
-			let value = typeof ref.value === 'object' ? get_String(ref.value) : ref.value;
-			obj[key] = value;
-		}
+  if (elem.attribute_count > 0)
+    for (let i = 0; i < elem.attribute_count; i++) {
+      let ref =
+        DataCenter.Attributes.data[elem.attributes[0]].data[
+          elem.attributes[1] + i
+        ];
+      let key = get_Name(ref);
+      let value =
+        typeof ref.value === "object" ? get_String(ref.value) : ref.value;
+      obj[key] = value;
+    }
 
-	if (elem.children_count > 0)
-		for (let i = 0; i < elem.children_count; i++) {
-			let ref = DataCenter.Elements.data[elem.children[0]].data[elem.children[1] + i];
-			let key = get_Name(ref);
-			if (!obj[key]) obj[key] = [];
-			obj[key].push(build(ref));
-		}
+  if (elem.children_count > 0)
+    for (let i = 0; i < elem.children_count; i++) {
+      let ref =
+        DataCenter.Elements.data[elem.children[0]].data[elem.children[1] + i];
+      let key = get_Name(ref);
+      if (!obj[key]) obj[key] = [];
+      obj[key].push(build(ref));
+    }
 
-	return obj;
+  return obj;
 }
 
 function get_String(ref) {
-	return DataCenter.Strings.map.get(`${ref[0]},${ref[1]}`);
+  return DataCenter.Strings.map.get(`${ref[0]},${ref[1]}`);
 }
 
 function get_Name(ref) {
-	if (ref.name_index === 0) return '__placeholder__';
-	ref = DataCenter.Names.addresses.data[ref.name_index - 1];
-	return DataCenter.Names.map.get(`${ref[0]},${ref[1]}`);
+  if (ref.name_index === 0) return "__placeholder__";
+  ref = DataCenter.Names.addresses.data[ref.name_index - 1];
+  return DataCenter.Names.map.get(`${ref[0]},${ref[1]}`);
 }
 
 function get_files(root) {
-	let child = [];
-	// console.log(get_Name(root), root);
-	for (let i = 0; i < root.children_count; i++) {
-		let ref = DataCenter.Elements.data[root.children[0]].data[root.children[1] + i];
-		child.push({ ref, name: get_Name(ref) });
-	}
+  let child = [];
+  // console.log(get_Name(root), root);
+  for (let i = 0; i < root.children_count; i++) {
+    let ref =
+      DataCenter.Elements.data[root.children[0]].data[root.children[1] + i];
+    child.push({ ref, name: get_Name(ref) });
+  }
 
-	let files = {};
-	for (let n of child) {
-		if (files[n.name]) {
-			if (!Array.isArray(files[n.name])) {
-				if (!fs.existsSync(path.join(output, n.name))) fs.mkdirSync(path.join(output, n.name), { recursive: true });
-				let temp = files[n.name];
-				files[n.name] = [];
-				files[n.name].push(temp);
-			}
-			files[n.name].push(n.ref);
-		} else {
-			files[n.name] = n.ref;
-		}
-	}
+  let files = {};
+  for (let n of child) {
+    if (files[n.name]) {
+      if (!Array.isArray(files[n.name])) {
+        if (!fs.existsSync(path.join(output, n.name)))
+          fs.mkdirSync(path.join(output, n.name), { recursive: true });
+        let temp = files[n.name];
+        files[n.name] = [];
+        files[n.name].push(temp);
+      }
+      files[n.name].push(n.ref);
+    } else {
+      files[n.name] = n.ref;
+    }
+  }
 
-	// fs.writeFileSync(path.join(__dirname, 'unpack.files.json'), JSON.stringify(files, null, '\t'));
-	return files;
+  // fs.writeFileSync(path.join(__dirname, 'unpack.files.json'), JSON.stringify(files, null, '\t'));
+  return files;
 }
